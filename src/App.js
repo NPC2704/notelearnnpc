5e5487e31d17cd8bf3062f29cfa730e63cc06aa9
Nguyễn Phước Công <88037699+NPC2704@users.noreply.github.com>
2023-05-02 23:41:15 +0700

    first up

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Landing from "./components/layout/Landing";
+import Auth from "./views/Auth";
+import AuthContextProvider from "./contexts/AuthContext";
+import Dashboard from "./views/Dashboard";
+import About from "./views/About";
+import ProtectedRoute from "./components/routing/ProtectedRoute";
+import PostContextProvider from "./contexts/PostContext";
+import NavbarMenu from "./components/layout/NavbarMenu";
+import AboutRoute from "../src/components/routing/AboutRoute";
+function App() {
+  return (
+    <AuthContextProvider>
+      <PostContextProvider>
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<Landing />} />
+            <Route exact path="/login" element={<Auth authRoutes="login" />} />
+            <Route
+              exact
+              path="/register"
+              element={<Auth authRoutes="register" />}
+            />
+
+            <Route exact path="/dashboard" element={<ProtectedRoute />} />
+            <Route exact path="/about" element={<AboutRoute />} />
+          </Routes>
+        </Router>
+      </PostContextProvider>
+    </AuthContextProvider>
+  );
+}
+
+export default App;
